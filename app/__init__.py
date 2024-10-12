8548f32a91930635555c9c307b8930c9094f9555
RichardJiang-collab <richardjiang736@Gmail.com>
2024-10-12 23:09:11 +0800

    Update fixes

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask import Flask
 from .extensions import db, migrate
 from .main import main as main_blueprint
 from flask_cors import CORS
-from whitenoise import WhiteNoise
-# from flask_session import Session  # Uncomment if using Flask-Session
+from whitenoise import WhiteNoise # type: ignore
+
 def create_app(config_name='production'):
     base_dir = os.path.abspath(os.path.dirname(__file__))
     static_folder = os.path.join(base_dir, '..', 'webflow_exporter_base')
@@ -18,22 +18,17 @@ def create_app(config_name='production'):
     app.config['SESSION_COOKIE_HTTPONLY'] = True
     app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 
-    # Initialize WhiteNoise
     app.wsgi_app = WhiteNoise(app.wsgi_app, root=static_folder, prefix='static/')
 
-    # Ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    # Initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
-
     app.register_blueprint(main_blueprint)
 
     with app.app_context():
         db.create_all()
-
     return app
